1fc5d551594e1cc459a3a0ce1eb44aa77fd87622
hyojae0812 <hjkiln@naver.com>
2023-07-10 20:17:33 +0900

    안드로이드 네이티브 API 통신 추가

--- android/app/src/main/kotlin/com/github/hyotatoFrappuccino/first_flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/hyotatoFrappuccino/first_flutter_app/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.github.hyotatoFrappuccino.first_flutter_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Build
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.flutter.dev/info"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+                .setMethodCallHandler { call, result ->
+                    if (call.method == "getDeviceInfo") {
+                        val deviceInfo = getDeviceInfo()
+                        result.success(deviceInfo)
+                    }
+                }
+    }
 }
---
+++ lib/native_example/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoApp(home: CupertinoNativeApp());
+    } else {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: NativeApp(),
+      );
+    }
+  }
+}
+
+class CupertinoNativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _NativeApp();
+  }
+}
+
+class NativeApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _NativeApp();
+}
+
+class _NativeApp extends State<NativeApp> {
+  // 안드로이드와 통신 채널로 사용할 MethodChannel
+  static const platform = const MethodChannel('com.flutter.dev/info');
+  String _deviceInfo = 'Unknown info';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Native 통신 예제'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            _deviceInfo,
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _getDeviceInfo();
+        },
+        child: Icon(Icons.get_app),
+      ),
+    );
+  }
+
+  Future<void> _getDeviceInfo() async {
+    String deviceInfo;
+    try {
+      // platform.invokeMethod > 네이티브 API 호출
+      final String result = await platform.invokeMethod('getDeviceInfo');
+      deviceInfo = 'Device info : $result';
+    } on PlatformException catch (e) {
+      deviceInfo = 'Failed to get Device info: ${e.message}';
+    }
+    setState(() {
+      _deviceInfo = deviceInfo;
+    });
+  }
+}
+
